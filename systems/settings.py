217a90551bb52eee88249c14c81f58446e77c00f
Math <philipponmathis@gmail.com>
2023-02-12 13:19:56 +0100

    Settings Possible values change

--- systems/settings.py
+++ systems/settings.py
@@ -1,14 +1,23 @@
+# file format:
+# settingName:settingValue:[possibleValues]
 
+# example:
+# width:800:[800,1020,1280,1600,1920]
 def readSetting(fcontent, settingName):
     for line in fcontent:
         if line.startswith(settingName):
-            return line.split(":")[1][:-1]
+            return line.split(":")[1]
     return None
 
 
-def formatSetting(settingName, settingValue):
-    return f"{settingName}:{settingValue}\n"
+def formatSetting(settingName, settingValue, settingPossibleValues):
+    return f"{settingName}:{settingValue}:{settingPossibleValues}]\n"
 
+def readPossibleValues(fcontent, settingName):
+    for line in fcontent:
+        if line.startswith(settingName):
+            return line.split(":")[2][1:-2].split(",")
+    return None
 
 def saveSettings(fHandle, content):
     for line in content:
@@ -24,6 +33,13 @@ class Settings:
             "fps": 60,
             "title": "Worms ékip 10",
         }
+        self.possibleValues = {
+            "width": [800, 1020, 1280, 1600, 1920],
+            "height": [600, 720, 900, 1080, 1440],
+            "fullscreen": [True, False],
+            "fps": [30, 60, 120],
+            "title": ["Worms ékip 10", "Worms ékip 10 - Fullscreen"],
+        }
         self.settings_types = {
             "width": int,
             "height": int,
@@ -37,10 +53,11 @@ class Settings:
             settings_file.close()
             for setting in self.settings:
                 self.settings[setting] = readSetting(fcontent, setting)
+                self.possibleValues[setting] = readPossibleValues(fcontent, setting)
         except FileNotFoundError:
             settings_file = open("settings.txt", "w")
             for setting in self.settings:
-                settings_file.write(formatSetting(setting, self.settings[setting]))
+                settings_file.write(formatSetting(setting, self.settings[setting], self.possibleValues[setting]))
             settings_file.close()
 
         for setting in self.settings:
